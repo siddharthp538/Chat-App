f99a6a834264aa661304fb394960bdf7fb3f454d
Siddharth Pandey <svpandey86@gmail.com>
2018-07-07 09:00:30 +0530

    Sending messages to Only people in our room

--- server/server.js
+++ server/server.js
@@ -41,18 +41,16 @@ io.on('connection',function(socket) {
 
   socket.on('createMessage' , function(message,callback){
      //console.log('createMessage' , message);
-     io.emit('newMessage' , generateMessage(message.from,message.text));
-     callback('This is from server');
-    /*
-    socket.broadcast.emit('newMessage' , {
-      from: message.from,
-      text: message.text,
-      createdAt : new Date().getTime()
-    });
-    */
+     var user = users.getUser(socket.id);
+     if(user && isRealString(message.text)){
+       io.to(user.room).emit('newMessage' , generateMessage(user.name,message.text));
+     }
+     callback();
   });
   socket.on('createLocationMessage' , function(coords){
-    io.emit('newLocationMessage' , generateLocationMessage('Admin', `${coords.latitude} , ${coords.longitude}`));
+    var user = users.getUser(socket.id);
+    if(user)
+      io.to(user.room).emit('newLocationMessage' , generateLocationMessage(user.name, `${coords.latitude} , ${coords.longitude}`));
   });
 
 
